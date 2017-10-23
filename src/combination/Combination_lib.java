47bb04def98b90f91e230cf74588f764dc7904a1
yuu10 <yuu10@DESKTOP-CK56FOC>
2017-10-23 17:38:21 +0900

    Combination

---
+++ src/combination/Combination_lib.java
@@ -0,0 +1,15 @@
+package combination;
+
+public class Combination_lib {
+	
+
+	public int getFactorial(int n) {
+     int f =1;
+     int i;
+     for(i=1;i<=n;i++) {
+         f=f*i;
+    	 //ŠKæŒvŽZ
+     }
+     return f;
+	}
+}
